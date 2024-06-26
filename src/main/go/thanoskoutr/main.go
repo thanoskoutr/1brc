1e00e27b64ce276bd49f733116905b5edfee5fed
thanoskoutr <koutroubas.thanos@gmail.com>
2024-06-26 17:45:29 +0300

    Calculate partial result measurements per worker and combine in the end
    Execution time ~= 9 minutes
    Locking is avoided
    True parallel execution, until results printing
    Slight performance improvement over previous implemention
    Need to check critical paths
    
    Done:
    - Each line is processed by a worker
    - Each worker calculates measurements and keeps a local result
    - All results all combined and aggregated on print

--- src/main/go/thanoskoutr/main.go
+++ src/main/go/thanoskoutr/main.go
@@ -21,9 +21,8 @@ import (
 // TODO: Delete run time calculations
 
 const (
-	WorkersCount    = 2048
-	LinesBufferSize = 10000
-	ShardCount      = 2048
+	WorkersCount    = 16
+	LinesBufferSize = 1000000 // Max: 1000000000 (not any difference), Min: 0 (significantly slower)
 )
 
 type Measurement struct {
@@ -37,82 +36,6 @@ type Stats struct {
 	Mean  float64
 	Count int
 	Sum   float64
-}
-
-type ShardedMeasurements struct {
-	Shards [ShardCount]map[string]*Stats
-	Mu     [ShardCount]sync.Mutex
-}
-
-func NewShardedMeasurements() *ShardedMeasurements {
-	sm := &ShardedMeasurements{}
-	for i := 0; i < ShardCount; i++ {
-		sm.Shards[i] = make(map[string]*Stats)
-	}
-	return sm
-}
-
-func (sm *ShardedMeasurements) getShard(key string) (map[string]*Stats, *sync.Mutex) {
-	hash := fnv32(key)
-	index := hash % ShardCount
-	return sm.Shards[index], &sm.Mu[index]
-}
-
-func (sm *ShardedMeasurements) update(location string, temperature float64) {
-	shard, mu := sm.getShard(location)
-	mu.Lock()
-	defer mu.Unlock()
-
-	// Calculate min, max, mean
-	if stats, exists := shard[location]; !exists {
-		shard[location] = &Stats{
-			Min:   temperature,
-			Max:   temperature,
-			Mean:  temperature,
-			Sum:   temperature,
-			Count: 1,
-		}
-	} else {
-		if temperature < stats.Min {
-			stats.Min = temperature
-		}
-		if temperature > stats.Max {
-			stats.Max = temperature
-		}
-		stats.Count++
-		stats.Sum += temperature
-		// Do not have to calculate the Mean here, do it on print
-	}
-}
-
-func (sm *ShardedMeasurements) combineMeasurements() map[string]*Stats {
-	combined := make(map[string]*Stats)
-	for i := 0; i < ShardCount; i++ {
-		sm.Mu[i].Lock()
-		for location, stats := range sm.Shards[i] {
-			if existingStats, exists := combined[location]; !exists {
-				combined[location] = &Stats{
-					Min:   stats.Min,
-					Max:   stats.Max,
-					Mean:  stats.Mean,
-					Sum:   stats.Sum,
-					Count: stats.Count,
-				}
-			} else {
-				if stats.Min < existingStats.Min {
-					existingStats.Min = stats.Min
-				}
-				if stats.Max > existingStats.Max {
-					existingStats.Max = stats.Max
-				}
-				existingStats.Count += stats.Count
-				existingStats.Sum += stats.Sum
-				// Do not have to calculate the Mean here, do it on print
-			}
-		}
-		sm.Mu[i].Unlock()
-	}
-	return combined
 }
 
 func formatMeasurements(measurements map[string]*Stats) string {
@@ -140,11 +63,16 @@ func formatMeasurements(measurements map[string]*Stats) string {
 	return sb.String()
 }
 
-func processFile(file *os.File) *ShardedMeasurements {
+func processFile(file *os.File) []map[string]*Stats {
 	// Read file line-by-line
 	var lineCount = 0
 	var countInterval = 1000000
-	shardedMeasurements := NewShardedMeasurements()
+	partialMeasurements := make([]map[string]*Stats, WorkersCount)
+
+	// Initialize partial results maps
+	for i := 0; i < WorkersCount; i++ {
+		partialMeasurements[i] = make(map[string]*Stats)
+	}
 
 	// Channel for reading line in parallel
 	var wg sync.WaitGroup
@@ -170,20 +98,21 @@ func processFile(file *os.File) *ShardedMeasurements {
 
 	for i := 0; i < WorkersCount; i++ {
 		wg.Add(1)
-		go func() {
+		go func(workerID int) {
 			defer wg.Done()
+			localResults := partialMeasurements[workerID]
 			for line := range lines {
-				processLine(line, shardedMeasurements)
+				processLine(line, localResults)
 			}
-		}()
+		}(i)
 	}
 
 	wg.Wait()
 
-	return shardedMeasurements
+	return partialMeasurements
 }
 
-func processLine(line string, shardedMeasurements *ShardedMeasurements) {
+func processLine(line string, measurements map[string]*Stats) {
 	parts := strings.Split(line, ";")
 	if len(parts) != 2 {
 		fmt.Printf("Skipping invalid line: %s\n", line)
@@ -196,24 +125,59 @@ func processLine(line string, shardedMeasurements *ShardedMeasurements) {
 		return
 	}
 
-	shardedMeasurements.update(location, temperature)
+	// Calculate min, max, mean
+	if stats, exists := measurements[location]; !exists {
+		measurements[location] = &Stats{
+			Min:   temperature,
+			Max:   temperature,
+			Mean:  temperature,
+			Sum:   temperature,
+			Count: 1,
+		}
+	} else {
+		if temperature < stats.Min {
+			stats.Min = temperature
+		}
+		if temperature > stats.Max {
+			stats.Max = temperature
+		}
+		stats.Count++
+		stats.Sum += temperature
+		// Do not have to calculate the Mean here, do it on print
+	}
+}
+
+func combineMeasurements(partialMeasurements []map[string]*Stats) map[string]*Stats {
+	combined := make(map[string]*Stats)
+	for _, partial := range partialMeasurements {
+		for location, stats := range partial {
+			if finalStats, exists := combined[location]; !exists {
+				combined[location] = &Stats{
+					Min:   stats.Min,
+					Max:   stats.Max,
+					Mean:  stats.Mean,
+					Sum:   stats.Sum,
+					Count: stats.Count,
+				}
+			} else {
+				if stats.Min < finalStats.Min {
+					finalStats.Min = stats.Min
+				}
+				if stats.Max > finalStats.Max {
+					finalStats.Max = stats.Max
+				}
+				finalStats.Count += stats.Count
+				finalStats.Sum += stats.Sum
+				// Do not have to calculate the Mean here, do it on print
+			}
+		}
+	}
+	return combined
 }
 
 // round rounds floats to 1 decimal place with 0.05 rounding up to 0.1
 func round(x float64) float64 {
 	return math.Floor((x+0.05)*10) / 10
-}
-
-// fnv32 implements the Fowler-Noll-Vo (FNV) algorithm for 32-bit numbers, which
-// a popular non-cryptographic hash algorithm, with good distribution and speed.
-func fnv32(key string) uint32 {
-	const prime32 = 16777619
-	hash := uint32(2166136261)
-	for i := 0; i < len(key); i++ {
-		hash *= prime32
-		hash ^= uint32(key[i])
-	}
-	return hash
 }
 
 func main() {
@@ -275,14 +239,15 @@ func main() {
 
 	// Process file
 	started = time.Now()
-	shardedMeasurements := processFile(file)
+	partialMeasurements := processFile(file)
 	finished = time.Now()
 	total = finished.Sub(started)
 	log.Printf("Total reading and calculation time: %v\n", total)
 
-	// Print results
+	// Combine results
 	started = time.Now()
-	measurements := shardedMeasurements.combineMeasurements()
+	measurements := combineMeasurements(partialMeasurements)
+	// Print results
 	results := formatMeasurements(measurements)
 	finished = time.Now()
 	total = finished.Sub(started)
